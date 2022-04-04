098b900fd8fb83a00c3da0a9fe6f9ffc7106a930
anthonypmarcel <anthonypmarcel@gmail.com>
2022-04-04 21:35:08 +0300

    Product details Component commit

---
+++ src/components/Products.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+import ProductDetails from "./ProductDetails";
+
+const Container = styled.div``;
+
+const Products = () => {
+  return (
+    <Container>
+      <ProductDetails />
+    </Container>
+  );
+};
+export default Products;
